7b5da2d5b9127bf02f2d82bdf626b0613d6fc40a
Lexadiaz120 <daura38@gmail.com>
2021-10-07 12:16:42 +0200

    deploy

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
- require("dotenv/config");
+require("dotenv/config");
 
 // ℹ️ Connects to the database
 require("./db");
 
+const path = require('path')
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
@@ -16,12 +17,13 @@ require("./config")(app);
 require("./config/session.config")(app)
 require("./config/cors.config")(app);
 
+app.use(express.static(path.join(__dirname, "public")))
+
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
 const allRoutes = require("./routes");
 app.use("/api", allRoutes);
 
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
+app.use((req, res) => res.sendFile(__dirname + "/public/index.html"));
 
 module.exports = app;
